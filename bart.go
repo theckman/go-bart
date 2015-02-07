02af3d0e1cc66358d1daa19417eaf475428990d9
Tim Heckman <t@heckman.io>
2015-02-07 03:49:36 -0800

    add an iniital package file

---
+++ bart.go
@@ -0,0 +1,7 @@
+// Copyright 2015 Tim Heckman. All rights reserved.
+// Use of this source code is governed by the BSD 3-Clause
+// license that can be found in the LICENSE file.
+
+// Package bart is used for interacting with the Bay Area Rapid Transit (BART) API.
+// This package is a work in progress.
+package bart
